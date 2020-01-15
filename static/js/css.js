27d0c4e869fbdcc58c264fc1086121b8306d2ba4
OsmiumOJ <OsmiumOJ@qq.com>
2020-01-15 22:48:09 +0800

    11.5.3

--- static/js/css.js
+++ static/js/css.js
@@ -1,7 +1,1 @@
 /* Css js */
-$(function() {
-    $('.add-grad-top-line, .add-grad-double-lines, .add-grad-border').append('<span class="grad-line-top"></span>');
-    $('.add-grad-bottom-line, .add-grad-double-lines, .add-grad-border').append('<span class="grad-line-bottom"></span>');
-    $('.add-grad-left-column, .add-grad-double-columns, .add-grad-border').append('<span class="grad-column-left"></span>');
-    $('.add-grad-right-column, .add-grad-double-columns, .add-grad-border').append('<span class="grad-column-right"></span>');
-});
