8bd243c543e659dfb18ee1dd0aed6c6ead701e9d
OsmiumOJ <OsmiumOJ@qq.com>
2020-01-30 15:46:11 +0800

    Big!

--- static/js/css.js
+++ static/js/css.js
@@ -1,3 +1,12 @@
 /* Css js */
 "use strict";
-$(function() {});
+$(function() {
+    var webHeight = Math.max(
+        $("main.main-part").height(),
+        $("main.full-part").height(),
+        $("div.ext-part").height()
+    );
+    $("main.main-part").height(webHeight);
+    $("main.full-part").height(webHeight);
+    $("div.ext-part").height(webHeight);
+});
