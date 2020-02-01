977612596b24b13084d3c4825caa7040b0920371
H-J-Granger <Hermionejoangranger@foxmail.com>
2020-02-01 21:20:10 +0800

    更改markdown编辑器宽度

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,7 @@ $(function() {
         success: function(res) {
             $("body").append(res);
             alert({
-                txt: "<div style=\"width: 100%; height: 50vh\"><div id=\"editor-md-1\" style=\"height: 100%\"><\/div><\/div>",
+                txt: "<div style=\"width: 100%; height: 50vh\"><div id=\"editor-md-1\" style=\"height: 100%; width: 100%\"><\/div><\/div>",
                 cap: "发表评论",
                 okTxt: "发&ensp;表",
                 hasCancel: true,
