e54ad6456eeaa08cf76396ba2e8d08996c2d2620
H-J-Granger <Hermionejoangranger@foxmail.com>
2020-02-01 20:32:09 +0800

    更改markdown编辑器

--- static/js/main.js
+++ static/js/main.js
@@ -4,13 +4,16 @@ $(function() {
         success: function(res) {
             $("body").append(res);
             alert({
-                txt: "<div style=\"width: 100%; height: 50vh\"><div id=\"editor-md-1\"><\/div><\/div>",
+                txt: "<div style=\"width: 100%; height: 50vh\"><div id=\"editor-md-1\" style=\"height: 100%\"><\/div><\/div>",
                 cap: "发表评论",
                 okTxt: "发&ensp;表",
                 hasCancel: true,
                 doShow: false
             });
             var editor = editormd("editor-md-1", {
+				width: "100%",
+				height: "100%",
+				tex: true,
                 path: "/static/editor.md/lib"
             });
         }
