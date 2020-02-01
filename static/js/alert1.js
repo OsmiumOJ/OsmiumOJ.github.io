f9dc2027e4943a4fe15dafa69d5ef83b1918c20c
OsmiumOJ <OsmiumOJ@qq.com>
2020-02-01 19:26:57 +0800

    Add alert-comment

--- static/js/alert1.js
+++ static/js/alert1.js
@@ -1,95 +1,57 @@
-function alert1(elmt, cap, OKWrd, OKfunc, hasCancel, CancelWrd, txtAlgn) {
-	$(".alert1Coverer").remove();
-	cap = cap ? cap : "提&ensp;示";
-	OKWrd = OKWrd ? OKWrd : "确&ensp;定";
-	CancelWrd = CancelWrd ? CancelWrd : "取&ensp;消";
-	txtAlgn = txtAlgn ? txtAlgn : "center";
-	hasCancel = hasCancel ? hasCancel : false;
-	$("body").append("<div class=\"alert1Coverer\"><style>.alert1 h1{font-size:30px;color:#483d8b;text-shadow:0px 0px 10px #aaaabb,0px 0px 20px #aaaabb,0px 0px 30px #aaaabb}.alert1 h2{font-size:24px;color:#483d8b;text-shadow:0px 0px 10px #aaaabb,0px 0px 20px #aaaabb,0px 0px 30px #aaaabb}.alert1 h3{font-size:21px;color:#483d8b;text-shadow:0px 0px 10px #aaaabb,0px 0px 20px #aaaabb,0px 0px 30px #aaaabb}.alert1 h4{font-size:19px;color:#483d8b;text-shadow:0px 0px 10px #aaaabb,0px 0px 20px #aaaabb,0px 0px 30px #aaaabb}.alert1 h5{font-size:18px;color:#483d8b;text-shadow:0px 0px 10px #aaaabb,0px 0px 20px #aaaabb,0px 0px 30px #aaaabb}.alert1 h6{font-size:17px;color:#483d8b;text-shadow:0px 0px 10px #aaaabb,0px 0px 20px #aaaabb,0px 0px 30px #aaaabb}.alert1 a{font-weight:600;transition-duration:0.4s}.alert1 a:link,.alert1 a:visited{color:#6a5acd;text-shadow:0px 0px 7px #888899,0px 0px 14px #888899,0px 0px 21px #888899}.alert1 a:hover,.alert1 a:active{color:#7b68ee;text-shadow:0px 0px 10px #aaaabb,0px 0px 20px #aaaabb,0px 0px 30px #aaaabb}.alert1 hr{border:1px solid rgba(0,0,0,0.2)}<\/style><div class=\"alert1Container\"><span style=\"font-size: 25px; color: #8787b4; text-shadow: 0px 0px 10px #aaaabb, 0px 0px 20px #aaaabb, 0px 0px 30px #aaaabb; text-weight: 600; margin-bottom: 7px;\">" + cap + "<\/span><div class=\"alert1\">" + elmt + "<\/div><button class=\"alert1Button\"id=\"alert1OK\">" + OKWrd + "<\/button>" + (hasCancel ? "<button class=\"alert1Button\"id=\"alert1Cancel\">" + CancelWrd + "<\/button><\/div>" : "<\/div><\/div>"));
-	$(".alert1Container").css({
-		backgroundColor: "rgba(255, 255, 255, 0.85)",
-		padding: "20px",
-		textAlign: "center",
-		borderRadius: "20px",
-		width: "65%",
-		transitionDuration: "0.4s",
-    });
-	$(".alert1Container").hover(
-		function() {
-			$(this).css({
-				boxShadow: "0px 0px 10px #aaaabb, 0px 0px 20px #aaaabb, 0px 0px 30px #aaaabb"
-			});
-		},
-		function() {
-			$(this).css({
-				boxShadow: "0px 0px 7px #888899, 0px 0px 14px #888899, 0px 0px 21px #888899"
-			});
+"use strict";
+var alert1On = false, alert1Chu = true;
+function alert1(args) {
+	if(alert1On) {
+		return;
+	}
+	if(args === undefined) {
+		if(alert1Chu) {
+			alert1Chu = false;
+			$("#alert1Container").css({display: "flex"});
 		}
-	);
-	$(".alert1Coverer").css({
-        position: "fixed",
-		left: "0",
-		right: "0",
-		top: "0",
-		bottom: "0",
-		backgroundColor: "rgba(0, 0, 0, 0.4)",
-		zIndex: "1048575",
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center"
-    });
-	$(".alert1").css({
-		border: "2px solid #777765",
-		background: "rgba(235, 235, 235, 0.2)",
-		textAlign: txtAlgn,
-		textOverflow: "ellipsis",
-		borderRadius: "7px",
-		boxShadow: "0 3px 4px 0 rgba(0, 0, 0, 0.24), 0 3px 9px 0 rgba(0, 0, 0, 0.19)"
-    });
-	$(".alert1Button").css({
-		fontWeight: "400",
-		outline: "medium",
-		backgroundColor: "rgba(105, 105, 150, 0.7)",
-		color: "#000000",
-		border: "none",
-		width: "100px",
-		height: "40px",
-		borderRadius: "20px",
-		textAlign: "center",
-		textDecoration: "none",
-		display: "inline-block",
-		fontSize: "16px",
-		margin: "7px 3px 0 3px",
-		cursor: "pointer",
-		transitionDuration: "0.4s",
-		boxShadow: "0 3px 4px 0 rgba(0, 0, 0, 0.24), 0 3px 9px 0 rgba(0, 0, 0, 0.19)"
+		$("#alert1Container").fadeIn();
+		return;
+	}
+	var txt = Boolean(args.txt) ? String(args.txt) : "内容";
+	var cap = Boolean(args.cap) ? String(args.cap) : "标题";
+	var okTxt = Boolean(args.okTxt) ? String(args.okTxt) : "确&ensp;定";
+	var hasCancel = Boolean(args.hasCancel);
+	var cancelTxt = Boolean(args.cancelTxt) ? String(args.cancelTxt) : "取&ensp;消";
+	var txtAlign = Boolean(args.txtAlign) ? String(args.txtAlign) : "center";
+	var okDo = Boolean(args.okDo) ? String(args.okDo) : null;
+	var doRemove = Boolean(args.doRemove) ? function() {
+		$("#alert1Container").erase();
+	} : null;
+	var doShow = args.doShow == undefined ? true : Boolean(args.doShow);
+	alert1On = doShow;
+	if(doShow && alert1Chu) {
+		alert1Chu = false;
+		$("#alert1Container").css({display: "flex"});
+	}
+	$("#alert1Txt").css({textAlign: txtAlign});
+	$("#alert1Txt").html(txt);
+	$("#alert1Cap").html(cap);
+	$("#alert1OkButton").html(okTxt);
+	$("#alert1OkButton").click(function() {
+		okDo;
+		doRemove;
+		$("#alert1Container").fadeOut();
+		alert1On = false;
 	});
-	$(".alert1Button").hover(
-		function() {
-			$(this).css({
-				fontWeight: "600",
-				backgroundColor: "#8787b4",
-				boxShadow: "0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19)"
-			});
-		},
-		function() {
-			$(this).css({
-				fontWeight: "400",
-				backgroundColor: "rgba(105, 105, 150, 0.7)",
-				boxShadow: "0 3px 4px 0 rgba(0, 0, 0, 0.24), 0 3px 9px 0 rgba(0, 0, 0, 0.19)"
-			});
-		}
-	);
-	$("#alert1OK").click(
-		function() {
-			$(".alert1Coverer").remove();
-			eval(OKfunc);
-		}
-	);
-	$("#alert1Cancel").click(
-		function() {
-			$(".alert1Coverer").remove();
-		}
-	);
+	$("#alert1CancelButton").remove();
+	if(hasCancel) {
+		$("#alert1Body").append("<button id=\"alert1CancelButton\" class=\"static-shad fill-3 alert1-button\"<\/button>");
+		$("#alert1CancelButton").html(cancelTxt);
+		$("#alert1CancelButton").click(function() {
+			doRemove;
+			$("#alert1Container").fadeOut();
+			alert1On = false;
+		});
+	}
+	if(doShow) {
+		$("#alert1Container").fadeIn();
+	}
 }
-window.alert = alert1;
+$(function() {
+	window.alert = alert1;
+});
--- static/js/main.js
+++ static/js/main.js
@@ -3,9 +3,16 @@ $(function() {
         url: "/web.dat",
         success: function(res) {
             $("body").append(res);
+            alert({
+                txt: "<div style=\"width: 100%; height: 50vh\"><div id=\"editor-md-1\"><\/div><\/div>",
+                cap: "发表评论",
+                okTxt: "发&ensp;表",
+                hasCancel: true,
+                doShow: false
+            });
+            var editor = editormd("editor-md-1", {
+                path: "/static/editor.md/lib"
+            });
         }
-    });
-    var editor = editormd("editor-md-1", {
-        path: "/static/editor.md/lib"
     });
 });
