76fbe16a5933fd326ddef556d18affcf99cd8823
lemonnyh <lemonnyh@outlook.com>
2020-02-07 21:59:49 +0800

    解决了烦人的editor.md，phew~

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,7 @@ $(function() {
         success: function(res) {
             $("body").append(res);
             alert({
-                txt: "<iframe src=\"\/editormd\" style=\"width: 100%; height: 50vh\" frameborder=\"0\" name=\"editor-md\"><\/iframe>",
+                txt: "<iframe style=\"width: 100%; height: 50vh\" src=\"\/editormd\" frameborder=\"0\" name=\"editor-md\"><\/iframe>",
                 cap: "发表评论",
                 okTxt: "发&ensp;表",
                 hasCancel: true,
