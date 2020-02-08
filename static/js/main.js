4ec503dd8e09598eaa605c35bf5fe529752b0fb1
lemonnyh <lemonnyh@outlook.com>
2020-02-08 15:29:12 +0800

    添加登录、注册以及修改了评论的UI

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,26 @@
+var changeStatus = false;
+function Change() {
+    if(changeStatus) {
+        $(".change:not(.change-show)").fadeToggle(400);
+        $(".change.change-show").delay(400).fadeToggle(400);
+    } else {
+        $(".change.change-show").fadeToggle(400);
+        $(".change:not(.change-show)").delay(400).fadeToggle(400);
+    }
+    changeStatus = !changeStatus;
+}
 $(function() {
     $.ajax({
         url: "/web.dat",
         success: function(res) {
             $("body").append(res);
+            editormd("comment-editor", {
+                width: "100%",
+                height: "50vh",
+                tex: true,
+                placeholder: "输入 Markdown",
+                path: "/static/editor.md/lib/"
+            });
             var xhr = new XMLHttpRequest();
             xhr.open('get', 'https://v1.hitokoto.cn?c=a&c=b&c=c&c=d');
             xhr.onreadystatechange = function () {
@@ -10,25 +28,9 @@ $(function() {
                     var data = JSON.parse(xhr.responseText);
                     $("#hitokoto-hitokoto").text(data.hitokoto);
                     $("#hitokoto-from").text('——' + data.from);
-                    var webHeight = Math.max(
-                        $("main.main-part").height(),
-                        $("main.full-part").height(),
-                        $("div.ext-part").height()
-                    );
-                    $("main.main-part").height(webHeight);
-                    $("main.full-part").height(webHeight);
-                    $("div.ext-part").height(webHeight);
                 }
             }
             xhr.send();
-            alert({
-                txt: "<iframe style=\"width: 100%; height: 50vh\" src=\"\/editormd\" frameborder=\"0\" name=\"editor-md\"><\/iframe>",
-                cap: "发表评论",
-                okTxt: "发&ensp;表",
-                hasCancel: true,
-                doShow: false,
-                txtAlign: "left"
-            });
         }
     });
 });
