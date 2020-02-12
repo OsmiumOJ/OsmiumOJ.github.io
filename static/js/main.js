60c88b26b18e05f610f9215b928b7dc632443640
lemonnyh <lemonnyh@outlook.com>
2020-02-12 18:00:31 +0800

    完善颜色主题

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,12 @@
+const uiData = {
+    colors: [
+        ["#008080", "#00bcbc", "#00ffff"],
+        ["#d94600", "#ea7500", "#ffaf60"],
+        ["#0000c6", "#0072e3", "#46a3ff"],
+        ["#ae00ae", "#ff359a", "#ff95ca"],
+        ["#3c3c3c", "#7b7b7b", "#bebebe"]
+    ]
+};
 var changeStatus = false;
 function Change() {
     if(changeStatus) {
@@ -9,11 +18,22 @@ function Change() {
     }
     changeStatus = !changeStatus;
 }
+function setColor(index) {
+    $(":root").css({cssText: "--color-0: " + uiData.colors[index][0] + "; --color-1: " + uiData.colors[index][1] + "; --color-2: " + uiData.colors[index][2]});
+    $.cookie("oojColor", String(index), {path: "/"});
+}
 $(function() {
     $.ajax({
         url: "/template.html",
         success: function(res) {
             $("body").append(res);
+            var colorsIndex = Boolean($.cookie("oojColor")) ? Number($.cookie("oojColor")) : 0;
+            console.log(colorsIndex);
+            var color = uiData.colors[colorsIndex % uiData.colors.length];
+            $(":root").css({cssText: "--color-0: " + color[0] + "; --color-1: " + color[1] + "; --color-2: " + color[2]});
+            for(var i = 0; i < uiData.colors.length; ++i) {
+                $("#color-selector").append("<i class=\"fa fa fa-dot-circle-o grad-txt\" style=\"cursor: pointer; background-image: linear-gradient(to bottom right, " + uiData.colors[i][0] + ", " + uiData.colors[i][1] + ")\" onclick=\"setColor(" + String(i) + ")\"><\/i>");
+            }
             alert({
                 doShow: false,
                 txt: "<div id=\"comment-editor\"><\/div>",
@@ -32,15 +52,4 @@ $(function() {
             });
         }
     });
-    const uiData = {
-        colors: [
-            ["#d94600", "#ea7500", "#ffa042"],
-            ["#008080", "#00bcbc", "#00ffff"],
-            ["#d94600", "#ea7500", "#ffa042"]
-        ]
-    }
-    var colorsIndex = Boolean($.cookie("oojColor")) ? Number($.cookie("oojColor")) : 0;
-    console.log(colorsIndex);
-    var color = UiData.colors[colorsIndex % UiData.colors.length];
-    $(":root").css({cssText: "--color-0: " + color[0] + "; --color-1: " + color[1] + "; --color-2: " + color[2]});
 });
