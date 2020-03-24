9ac9842cf668e7f697f205aaf825834d8a627d72
H-J-Granger <Hermionejoangranger@foxmail.com>
2020-03-24 20:17:23 +0800

    使登录注册按钮可以导入到正确的地址

--- script/main.js
+++ script/main.js
@@ -1,1 +1,11 @@
 "use strict";
+$(document).ready(() => {
+	$('#sign-in-button').click((event) => { 
+		event.preventDefault();
+		window.open(`/login?href=${window.location.pathname}`, '_self');
+	});
+	$('#sign-up-button').click((event) => { 
+		event.preventDefault();
+		window.open(`/register?href=${window.location.pathname}`, '_self');
+	});
+});
