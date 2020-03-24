c1a67785cd752ad78fa54521d038eef8a19d4228
H-J-Granger <Hermionejoangranger@foxmail.com>
2020-03-24 18:19:57 +0800

    将登录注册按钮改回button

--- script/main.js
+++ script/main.js
@@ -1,9 +1,1 @@
-$('#sign-in-button').click((e) => { 
-	e.preventDefault();
-	window.open(`/login?href=${encodeURIComponent(window.location.pathname)}`, '_self');
-});
-
-$('#sign-up-button').click((e) => { 
-	e.preventDefault();
-	window.open(`/register?href=${encodeURIComponent(window.location.pathname)}`, '_self');
-});
+"use strict";
