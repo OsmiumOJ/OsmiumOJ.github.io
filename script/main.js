3e958f1e8ef624d3124f3b22a513e782f7f7ccad
H-J-Granger <Hermionejoangranger@foxmail.com>
2020-03-24 20:21:21 +0800

    添加登录注册按钮跳转链接的encodeURIComponent

--- script/main.js
+++ script/main.js
@@ -2,10 +2,10 @@
 $(document).ready(() => {
 	$('#sign-in-button').click((event) => { 
 		event.preventDefault();
-		window.open(`/login?href=${window.location.pathname}`, '_self');
+		window.open(`/login?href=${encodeURIComponent(window.location.pathname)}`, '_self');
 	});
 	$('#sign-up-button').click((event) => { 
 		event.preventDefault();
-		window.open(`/register?href=${window.location.pathname}`, '_self');
+		window.open(`/register?href=${encodeURIComponent(window.location.pathname)}`, '_self');
 	});
 });
