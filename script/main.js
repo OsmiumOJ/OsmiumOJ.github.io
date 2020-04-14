694fdba8f9df023028698ca8f5201d4eed542648
H-J-Granger <Hermionejoangranger@foxmail.com>
2020-04-14 13:59:05 +0800

    炮决多语言

--- script/main.js
+++ script/main.js
@@ -1,92 +1,5 @@
 "use strict";
 
-// TODO: throw it into a seperate JSON file.
-const languageLibrary = {
-	"simplifyChinese": {
-		"mainPage": "首页",
-		"公告": "公告",
-		"近期比赛": "近期比赛",
-		"比赛列表": "比赛列表",
-		"一言": "一言",
-		"加载中……": "加载中……",
-		"——加载中……": "——加载中……",
-		"当前rating：": "当前rating：",
-		"最高rating：": "最高rating：",
-		"私信": "私信",
-		"通知": "通知",
-		"收藏": "收藏",
-		"博客": "博客",
-		"Rating变化": "Rating变化",
-		"设置": "设置"
-	},
-	"traditionalChinese": {
-		"mainPage": "首頁",
-		"公告": "公告",
-		"近期比赛": "近期比賽",
-		"比赛列表": "比賽列表",
-		"一言": "一言",
-		"加载中……": "加載中……",
-		"——加载中……": "——加載中……",
-		"当前rating：": "當前rating：",
-		"最高rating：": "最高rating：",
-		"私信": "私信",
-		"通知": "通知",
-		"收藏": "收藏",
-		"博客": "博客",
-		"Rating变化": "Rating變化",
-		"设置": "設置"
-	},
-	"english": {
-		"mainPage": "Main page",
-		"公告": "Announcement",
-		"近期比赛": "Upcoming contests",
-		"比赛列表": "Contest List",
-		"一言": "Hitokoto",
-		"加载中……": "Loading...",
-		"——加载中……": "- Loading...",
-		"当前rating：": "Current rating：",
-		"最高rating：": "Highest rating：",
-		"私信": "Message",
-		"通知": "Notification",
-		"收藏": "Favorite",
-		"博客": "Blog",
-		"Rating变化": "Rating changes",
-		"设置": "Settings"
-	}
-};
-
-// TODO: Merge them.
-let languageModifierForDiv = new Vue({
-	el: 'div',
-	data: localStorage.getItem('language-library') === null ? languageLibrary.simplifyChinese : JSON.parse(localStorage.getItem('language-library'))
-});
-let languageModifierForHeader = new Vue({
-	el: 'header',
-	data: localStorage.getItem('language-library') === null ? languageLibrary.simplifyChinese : JSON.parse(localStorage.getItem('language-library'))
-});
-let languageModifierForFooter = new Vue({
-	el: 'footer',
-	data: localStorage.getItem('language-library') === null ? languageLibrary.simplifyChinese : JSON.parse(localStorage.getItem('language-library'))
-});
-
-function modifyLanguage(lang) {
-	if (localStorage.getItem('language') === lang) {
-		return;
-	}
-	let tmp;
-	if (lang === 'simplify-chinese') {
-		tmp = languageLibrary.simplifyChinese;
-	}
-	if (lang === 'traditional-chinese') {
-		tmp = languageLibrary.traditionalChinese;
-	}
-	if (lang === 'english') {
-		tmp = languageLibrary.english;
-	}
-	localStorage.setItem('language-library', JSON.stringify(tmp));
-}
-
-
 $(() => {
 	$('#sign-in-button').click((event) => { 
 		event.preventDefault();
@@ -95,21 +8,5 @@ $(() => {
 	$('#sign-up-button').click((event) => { 
 		event.preventDefault();
 		window.open(`/register?href=${encodeURIComponent(window.location.pathname)}`, '_self');
-	});
-	new Vue({
-		el: 'div',
-		data: localStorage.getItem('language-library') === null ? languageLibrary.simplifyChinese : JSON.parse(localStorage.getItem('language-library'))
-	});
-	new Vue({
-		el: 'header',
-		data: localStorage.getItem('language-library') === null ? languageLibrary.simplifyChinese : JSON.parse(localStorage.getItem('language-library'))
-	});
-	new Vue({
-		el: 'title',
-		data: localStorage.getItem('language-library') === null ? languageLibrary.simplifyChinese : JSON.parse(localStorage.getItem('language-library'))
-	});
-	new Vue({
-		el: 'footer',
-		data: localStorage.getItem('language-library') === null ? languageLibrary.simplifyChinese : JSON.parse(localStorage.getItem('language-library'))
 	});
 });
